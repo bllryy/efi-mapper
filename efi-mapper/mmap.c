c0a421f97ba92488d670ead6f32cd5f732c80bf4
lily <bllryy@proton.me>
2025-08-30 00:01:32 -0400

    mmap

---
+++ efi-mapper/mmap.c
@@ -0,0 +1,10 @@
+/*
+implements the actual memory mapping logic that loads a driver into memory without going through the normal Windows driver loading process
+
+also need a header file for function declarations and structure definitions
+*/
+
+
+#include "util.h"
+#include "mmap.h"
+
